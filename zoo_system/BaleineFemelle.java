062d4dcc030ffcf83b33803809222cef22f3e2d2
AlexChantraine <zeldaokami118@gmail.com>
2016-10-29 17:32:50 +0200

    correction nom variable + methode

--- zoo_system/BaleineFemelle.java
+++ zoo_system/BaleineFemelle.java
@@ -8,13 +8,13 @@ public class BaleineFemelle extends Baleine implements MammifereFemelle{
 
 	public void mettreBas() {
 		if(!(this.getEnclosResidence().isFull())){
-			double randomAigle = Math.random();
+			double randomBaleine = Math.random();
 			double randomPoids = this.arrondiDecimals(Math.random() * (3000 - 2500), 1);
 			double randomTaille = this.arrondiDecimals(Math.random() * (7 - 5), 1);
-			if(randomAigle <= 0.5){
-				this.getEnclosResidence().ajouterAnimal(AnimalFactory.getAigleMale(randomPoids, randomTaille));
+			if(randomBaleine <= 0.5){
+				this.getEnclosResidence().ajouterAnimal(AnimalFactory.getBaleineMale(randomPoids, randomTaille));
 			}else{
-				this.getEnclosResidence().ajouterAnimal(AnimalFactory.getAigleFemelle(randomPoids, randomTaille));
+				this.getEnclosResidence().ajouterAnimal(AnimalFactory.getBaleineFemelle(randomPoids, randomTaille));
 			}
 		}
 	}//mettreBas()
