1e05cd088600b946237fb456a7b7b75b3e249144
Joris Brandt <jorisbrandt@gmail.com>
2016-11-04 15:03:12 +0100

    Rajout javadoc pour vues enclos

--- zoo_system/VueEnclos.java
+++ zoo_system/VueEnclos.java
@@ -1,7 +1,9 @@
 package zoo_system;
 
 public abstract class VueEnclos<T extends Enclos<? extends Animal>> extends Vue<T>{
-
+	/**
+	 * Tri les animaux dans un enclos selon le paramètre choisi
+	 */
 	public void effectuerTri(){
 		System.out.println(this.getModel().effectuerTri());
 	}
